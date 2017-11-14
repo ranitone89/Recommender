850e7f74ef611168e307654593b8c68cf80ea81b
ranitone89 <ranitone89.email@test.com>
2017-11-14 11:08:16 +0100

    favorite actors

--- build/web/js/db/submit.js
+++ build/web/js/db/submit.js
@@ -5,15 +5,21 @@
  */
 
 $(document).ready(function(){
-    $("#submit").on('click', function(){
+    
+    var username = "";
+    var email = "";
+    var password_1 = "";
+    var password_2 = "";
+    var genres = [];
+    var actorList = [];
+    
+    $("#submit1").on('click', function(){
         var checked = $(".genre input:checked").length > 3;
         
-        var genres = [];
-        
-        var username = $("#username_reg").val();
-        var email = $("#email_reg").val();
-        var password_1 = $("#password_1_reg").val();
-        var password_2 = $("#password_2_reg").val();
+        username = $("#username_reg").val();
+        email = $("#email_reg").val();
+        password_1 = $("#password_1_reg").val();
+        password_2 = $("#password_2_reg").val();
         
 
         if (!checked){
@@ -24,9 +30,25 @@ $(document).ready(function(){
         else{
             $('.genre input:checked').each(function() {
                 genres.push($(this).val());
+                
             });
             alert(genres);
-            
+            $('.formular-container-genre').css("display","none");
+            $('.formular-container-actor').css("display","block");
+        }
+    });
+    
+    $("#submit2").on('click', function(){
+        var actors = removeLastComma($('#favoriteActors').val());
+        actorList = covertToArray(actors,'a');
+
+        if(actorList.length==0){
+             $('#messageQuastionaireActor').css("display","block");
+             $('#messageQuastionaireActor').html("<font color='red'>Insert at least the Name of one Actor </font>")
+             return;
+         }
+        
+        else{
             $.ajax({
                 url : "RegisterServlet",
                 type : "GET",
@@ -50,6 +72,7 @@ $(document).ready(function(){
         }
     });
     
+    
     //function to display message to the user
     function showMessage(results){
         if(results == 'SUCCESS'){
@@ -58,5 +81,103 @@ $(document).ready(function(){
             $('#messageDiv').html("<font color='red'>Username or password incorrect </font>")
         }
     }
+    
+    /**
+     * 
+     * @param {type} array
+     * @param {type} string
+     * @returns {Array}
+     */
+    function covertToArray(array, string)
+    {
+        var theArray = '';
+
+        if(string =='a'){
+            theArray = array.split(", ");
+            for (var i = 0; i < theArray.length; i++) {
+                if (theArray[i].length > 0) {
+                    var tmp = theArray[i].split(" ");
+                    if(tmp.length>=3){
+                        if(tmp[tmp.length-1]=="Jr." || tmp[tmp.length-1]=="Sr."){
+                            theArray[i] = tmp[1] + " " + tmp[tmp.length-1] + ", " + tmp[0];
+                        }
+                        if(tmp[1]=="De"){
+                            theArray[i] = tmp[1]+ " " +tmp[tmp.length-1]+ ", " + tmp[0];
+                        }
+                        if(tmp[1].indexOf(".")> -1){
+                            theArray[i] = tmp[tmp.length-1]+ ", " + tmp[0]+ " "+tmp[1];
+                        }
+                    }
+                    else{
+                        theArray[i] = tmp[tmp.length-1]+ ", " + tmp[0];
+                    }
+                }
+            }
+
+        }
+
+        if(string =='g'){
+            theArray = array.split(",");
+            for (var i = 0; i < theArray.length; i++) {
+                if(theArray[i]!=="") {
+                  var tmp = theArray[i];
+                  theArray[i] = tmp;
+                }
+            }
+        }
+
+        if(string =='r'){
+            theArray = array.split(", ");
+            for (var i = 0; i < theArray.length; i++) {
+                if(theArray[i]!=="") {
+                    var tmp = theArray[i].substring(0, theArray[i].indexOf('('));
+                    tmp = tmp.replace('[','');
+                    theArray[i] = tmp;
+                }
+            }
+        }
+
+        return unique(removeEmptyElements(theArray));
+    }
+
+    /**
+     * 
+     * @param {type} list
+     * @returns {Array}
+     */
+    function unique(list) {
+      var result = [];
+      $.each(list, function(i, e) {
+        if ($.inArray(e, result) == -1) result.push(e);
+      });
+      return result;
+    }
+
+    /**
+     * 
+     * @param {type} str
+     * @returns {unresolved}
+     */
+    function removeLastComma(str) {
+       return str.replace(/,(\s+)?$/, '');
+    }
+
+    /**
+     * 
+     * @param {type} array
+     * @returns {Array}
+     */
+    function removeEmptyElements(array) {
+        var newArray = [];
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] !== "" && array[i] !== null) {
+                newArray.push(array[i]);
+            }
+        }
+        return newArray;
+    }    
+    
+    
+    
 });
 
--- web/js/actorField.js
+++ web/js/actorField.js
@@ -19,5 +19,19 @@ $(document).ready(function() {
         }
         $("#actors").attr('rows', '1');
     });
-    //$("#actors").attr('rows', '1');
+    
+    $("#favoriteActors").on('change keyup paste', function(){
+        var OriginalHeight = $(".dropdown").height();
+        if (!$.trim($(this).val())) {
+            $(this).css('height','');
+            $('.dropdown dt a').css('min-height','');
+            $(this).css('padding','13 19 12 10');
+        }
+        else{
+            $(this).css('height','auto');
+            $(this).height(this.scrollHeight-15);
+            $('.dropdown dt a').css('min-height',this.scrollHeight-15);
+        }
+        $("#favoriteActors").attr('rows', '1');
+    });
 }); 
