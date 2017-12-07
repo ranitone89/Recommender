b0c92a2727330c6bc205d5b9140b508e585f5fb5
Nemanja Ranitovic <qi11102@tmv.intra>
2017-12-07 16:29:21 +0100

    Refactoring start.html

--- web/js/tab.js
+++ web/js/tab.js
@@ -1,5 +1,5 @@
 
-/*activeTab, backToTab, searchTab*/
+/*activeTab, backToTab, TabNavigatio*/
 /*Open tab*/
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
@@ -50,7 +50,65 @@ function backToTab(evt) {
 
 
 $(document).ready(function() {
-        $(document).on("click", ".tab-back-nav", function(event){
+    
+    /*Search Tab*/
+    var clusterClicked = false;
+    $(document).on("click", ".button-container .open", function(event){
+        if ( $(this).hasClass('active')==false){
+            $(this).removeClass("open");
+            $(this).addClass("closee");
+            $('.search').hide();
+            $('.search-tab-close').hide();
+        }
+   });
+   
+    $(document).on("click", ".button-container .closee", function(event){
+        if ( $(this).hasClass('active')==false){
+            $(this).removeClass("closee");
+            $(this).addClass("open");
+            $('.search').show();
+            $('.search-tab-close').show();
+
+            if(clusterClicked==false){
+               $('.clusterbtn').trigger('click');
+               clusterClicked=true;
+            }
+        }
+   });
+   
+   $(document).on("click", ".search .search-tab-close", function(event){
+        $('.button-container .open').trigger("click");
+        $('.clusterbtn').css("display","block");
+   });
+   
+   
+   $(document).on("click","#newBtn", function(event){
+        $('#id01 .tab').css("display","block");
+        $('#id01 .search-tab').css("display","block");
+        $('#defineBtn').css("display","block"); 
+        $('.clusterbtn').css("display","none");
+        $('#id01 .tab').css("display","block");
+        $('.scen_param').css("display","block");
+        $('.scen_desc').css("display","block");
+        $('hr').css("display","block");
+        $('#description').css("display","block");
+        $('.searchParameter').css("display","block");
+        $('.search_param').css("display","block");
+   });
+   
+   $(document).on("click","#id01 .search-tab-close", function(event){
+        $('.clusterbtn').css("display","block");
+        $('#defineBtn').css("display","none");
+        $('#id01 .tab').css("display","none");
+        $('#messageEval').css("display","none");
+        $('.scen_param').css("display","none");
+        $('.scen_desc').css("display","none");
+        $('hr').css("display","none");
+        $('#description').css("display","none");
+   });
+   
+   /*Tab Navigation*/
+   $(document).on("click", ".tab-back-nav", function(event){
         var $movie = $(this).closest('.Movie');
         $('.title').css('display', 'none');
         $('.methods').css('display', 'none');
@@ -81,4 +139,20 @@ $(document).ready(function() {
         $('.tab-back-nav').hide();
         $('.recom-text').hide();
    });
+   
+   /*CloseSearch*/
+   $(document).on("click", ".search .search-tab-close", function(event){
+        $('.search').hide();
+        $('.search .search-tab-close').hide();
+   });
+   
+   $(document).on("click", ".statistics-close", function(event){
+        $('.cluster2').css('display','block');
+        $('#method2').css('display','block');
+        $('#statistics #statcs_cl2').css('display', 'none');
+        $('#statistics #statcs_cl1').css('display', 'none');
+        $('#statistics #statcs_cl3').css('display', 'none');
+        $('#statistics').css('display', 'none');
+   });
+   
 }); 
